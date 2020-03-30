bbdfcc633d2e669bcdc933ab62697f035add0a58
Austin Wilson <austin@newcoastventures.com>
2020-03-30 18:41:21 +0100

    remove unneeded files and add css baseline"

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
 import * as serviceWorker from "./serviceWorker";
 import App from "./components/App";
 import Firebase, { FirebaseContext } from "./components/Firebase";
+
 ReactDOM.render(
   <FirebaseContext.Provider value={new Firebase()}>
+    <CssBaseline />
     <App />
   </FirebaseContext.Provider>,
   document.getElementById("root")
 );
+
 serviceWorker.unregister();
