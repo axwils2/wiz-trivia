9a3a26c3fb403ec3aa6c47e8d424e19d386423b1
Austin Wilson <austin@newcoastventures.com>
2020-05-09 12:04:18 +0100

    remove ununsed divider

--- src/pages/ActiveTriviaSessionPage/components/PlayerActiveSession/components/LeaderBoardSideDrawer.jsx
+++ src/pages/ActiveTriviaSessionPage/components/PlayerActiveSession/components/LeaderBoardSideDrawer.jsx
@@ -2,7 +2,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
-import Divider from "@material-ui/core/Divider";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
